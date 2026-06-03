c880342034d6d4c707f76fbd6075dfa8cb80ad8c
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-06-03 13:17:18 +0500

    feat: Comment out input handling and previous value display in Docs component

--- src/Components/Pages/Docs.jsx
+++ src/Components/Pages/Docs.jsx
@@ -3,17 +3,17 @@ import { ProjectTheme } from './ChangTheme'
 
 function Docs() {
     const {theme} = useContext(ProjectTheme)
-    const [inputValue,setInputValue] = useState('')
-    const previousValue = useRef()
-    useEffect(()=>{
-      previousValue.current.value = inputValue
-    },[inputValue])
+    // const [inputValue,setInputValue] = useState('')
+    // const previousValue = useRef()
+    // useEffect(()=>{
+    //   previousValue.current.value = inputValue
+    // },[inputValue])
   return (
     <div style={{background:theme==='light'?'white':'#222',color:theme==='light'?'black':'white',height:'100vh'}}>
       <h1 style={{padding:'20px',margin:'0px'}}>Docs</h1>
-    <input type='text' value={inputValue} onChange={(e)=>setInputValue(e.target.value)}></input>
+    {/* <input type='text' value={inputValue} onChange={(e)=>setInputValue(e.target.value)}></input>
     <h1>Current Value : {inputValue}</h1>
-    <h1>Previous Value : {previousValue.current.value}</h1>
+    <h1>Previous Value : {previousValue.current.value}</h1> */}
     </div>
 
   )

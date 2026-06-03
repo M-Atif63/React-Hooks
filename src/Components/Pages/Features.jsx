3641b8e21ef6682c8e315f70069e2250b6735064
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-06-03 12:46:21 +0500

    feat: Refactor Features component by adding input field border style and cleaning up code

--- src/Components/Pages/Features.jsx
+++ src/Components/Pages/Features.jsx
@@ -4,7 +4,6 @@ import { ProjectTheme } from './ChangTheme'
 function Features() {
   const [inputValue,setInputValue] = useState('')
   const { theme } = useContext(ProjectTheme)
-  
   const count = useRef(0)
   useEffect(()=>{
    count.current = count.current + 1; 
@@ -12,11 +11,10 @@ function Features() {
   return (
     <div style={{ background: theme === 'light' ? 'white' : '#222', color: theme === 'light' ? 'black' : 'white', height: '100vh' }}>
       <h1 style={{ padding: '20px', margin: '0px' }}>Features</h1>
+      {/* --useRef--  */}
     <p>Type in the input field:</p>
-    <input type='text' value={inputValue} onChange={(e)=>setInputValue(e.target.value)}></input>
+    <input type='text' value={inputValue} onChange={(e)=>setInputValue(e.target.value)} style={{border:"2px solid black"}}></input>
     <h1>Render Count : {count.current}</h1>
-    
-    
     </div>
 
 

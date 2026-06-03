f57d8c769a826b4ea43f2610580b66ddcca16d5b
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-06-03 12:42:18 +0500

    feat: Add input field and render count to Features component

--- src/Components/Pages/Features.jsx
+++ src/Components/Pages/Features.jsx
@@ -1,12 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { ProjectTheme } from './ChangTheme'
 
 function Features() {
-    const {theme} = useContext(ProjectTheme)
+  const [inputValue,setInputValue] = useState('')
+  const { theme } = useContext(ProjectTheme)
+  
+  const count = useRef(0)
+  useEffect(()=>{
+   count.current = count.current + 1; 
+  },[inputValue])
   return (
-    <div style={{background:theme==='light'?'white':'#222',color:theme==='light'?'black':'white',height:'100vh'}}>
-      <h1 style={{padding:'20px',margin:'0px'}}>Features</h1>
+    <div style={{ background: theme === 'light' ? 'white' : '#222', color: theme === 'light' ? 'black' : 'white', height: '100vh' }}>
+      <h1 style={{ padding: '20px', margin: '0px' }}>Features</h1>
+    <p>Type in the input field:</p>
+    <input type='text' value={inputValue} onChange={(e)=>setInputValue(e.target.value)}></input>
+    <h1>Render Count : {count.current}</h1>
+    
+    
     </div>
+
+
+
+
+
   )
 }
 

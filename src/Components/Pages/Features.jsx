2cc36b69718b58e8565c036edd809405a798af1d
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-06-03 12:58:47 +0500

    feat: Update Features component layout and input field styling for improved UI

--- src/Components/Pages/Features.jsx
+++ src/Components/Pages/Features.jsx
@@ -12,10 +12,11 @@ function Features() {
     <div style={{ background: theme === 'light' ? 'white' : '#222', color: theme === 'light' ? 'black' : 'white', height: '100vh' }}>
       <h1 style={{ padding: '20px', margin: '0px' }}>Features</h1>
       {/* --useRef--  */}
-    <p>Type in the input field:</p>
-    <input type='text' value={inputValue} onChange={(e)=>setInputValue(e.target.value)} style={{border:"2px solid black"}}></input>
+      <center>
+    <h3>Type in the input field:</h3>
+    <input type='text' value={inputValue} onChange={(e)=>setInputValue(e.target.value)} style={{border:theme==='light'?"1px solid black":"1px solid white",backgroundColor:theme==="light"?'white':'#222',width:'500px',height:'30px',borderRadius:'5px',textAlign:'center',fontSize:'25px',outline:'none',color:theme==='light'?'black':'white'}}></input>
     <h1>Render Count : {count.current}</h1>
-    </div>
+    </center></div>
 
 
 

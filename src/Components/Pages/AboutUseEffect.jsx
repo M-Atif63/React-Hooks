cf0973c592e68fc8edb343cd7bbca5dfc23a8c03
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-06-14 01:47:49 +0500

    feat: Replace Contact component with AboutUseEffect and update routing

---
+++ src/Components/Pages/AboutUseEffect.jsx
@@ -0,0 +1,26 @@
+// import React, { useContext } from 'react'
+// import { ProjectTheme } from './ChangTheme'
+
+// function AboutUseEffect() {
+//   const {theme} = useContext(ProjectTheme)
+//   return (
+//     <div style={{background:theme==='light'?'white':'#222',color:theme==='light'?'black':'white',height:'100vh'}}>
+//       <h1 style={{padding:'20px',margin:'0px'}}>About UseEffect</h1>
+//     </div>
+//   )
+// }
+
+// export default AboutUseEffect
+
+
+import React from 'react'
+
+function AboutUseEffect() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AboutUseEffect

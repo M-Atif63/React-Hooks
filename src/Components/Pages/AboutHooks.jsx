65eb6b1b76850ed53667d366094c43cd9d0f9ead
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-06-14 02:12:05 +0500

    feat: Update AboutHooks component with introductory content on Hooks

--- src/Components/Pages/AboutHooks.jsx
+++ src/Components/Pages/AboutHooks.jsx
@@ -15,11 +15,18 @@
 import React from 'react'
 
 function AboutHooks() {
-
-
   return (
     <div>
-      
+
+      --<p>Q1:</p>
+      <p>Hooks kise kahte hain?</p>
+      <hr />
+      <p>Ans:</p>
+      <p>Hooks classes components ko use kiye bina he functional components me apko react ki powers like [state,lifecycle,effect etc] ko 
+        access karne ki permission deta hai</p>
+        <br />
+      <p>agar asaan alfaaz me samjha jaye to, Hooks un Power full functions ko kehte hain jo aap ko is baat ki ijazat dete hain ke aap 
+        react ki power like [state,lifecycle,effect etc] ko kisi bhi functional components me use kar sakte hain without use class</p>
     </div>
   )
 }

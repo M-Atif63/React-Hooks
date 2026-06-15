d1e7d74eb0a53dbc4acadb019a96161a0af4183e
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-06-15 12:59:52 +0500

    feat: Add comments explaining controlled and uncontrolled components in Docs.jsx

--- src/Components/Pages/Docs.jsx
+++ src/Components/Pages/Docs.jsx
@@ -8,7 +8,23 @@ function Docs() {
     // useEffect(()=>{
     //   previousValue.current.value = inputValue
     // },[inputValue])
-  return (
+
+    // -- React Form--
+
+    // Q1) React mein controlled aur uncontrolled components kya hote hain?
+    // Ans) Controlled Component : 
+    //                           har wo input element controlled component hota hai jis 
+    // ki value React state(useState) se control ki jati hai, aur us value mein koi bhi
+    // tabdeeli event handler ke zariye manage hoti hai jo state ko update karte hain
+    
+    // Examples of controlled components:
+    // input ,textarea ,select elements controlled components ke examples hain
+
+    // Uncontrolled Component :
+    //                           jo component controlled component ke ilawa ho usko
+    //  uncontrolled component kaha jata hai
+
+    return (
     <div style={{background:theme==='light'?'white':'#222',color:theme==='light'?'black':'white',height:'100vh'}}>
       <h1 style={{padding:'20px',margin:'0px'}}>Docs</h1>
     {/* <input type='text' value={inputValue} onChange={(e)=>setInputValue(e.target.value)}></input>
